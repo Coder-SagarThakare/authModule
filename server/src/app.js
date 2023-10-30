de661d4f155212b48c40e7327db72c2ebb0c22c3
AM-SagarThakare <sagar.thakare@angularminds.in>
2023-10-30 15:44:44 +0530

    authlimiter added

--- server/src/app.js
+++ server/src/app.js
@@ -2,20 +2,26 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const routes = require('./routes')
+const routes = require("./routes");
+const config = require("./config/config");
+const authLimiter = require("./middlewares/rateLimiter");
 
 // ------------------  MIDDLEWARES  ----------------------------
 
 // JSON requests are received as plain text. We need to parse the json request body.
 app.use(bodyParser.json());
 
+// Parse urlencoded request body if provided with any of the requests
+app.use(express.urlencoded({ extended: true }));
+
 // Enable cors to accept requests from any frontend domain,
 app.use(cors());
 
+if (config.env == "production") {
+  app.use("/auth", authLimiter);
+}
+
 // Define routes index in separate file.
 app.use("/", routes);
-
-// Parse urlencoded request body if provided with any of the requests
-app.use(express.urlencoded({ extended: true }));
 
 module.exports = app;
