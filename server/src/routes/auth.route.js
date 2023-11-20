e253ea3d4a3d7c1b10c40a4cfa89b81da3b84f03
AM-SagarThakare <sagar.thakare@angularminds.in>
2023-11-20 17:24:35 +0530

    validate function added

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -1,6 +1,8 @@
 const logger = require("../config/logger");
 const { authController } = require("../controllers");
 const captcha = require("../middlewares/captcha");
+const validate = require("../middlewares/validate");
+const { authValidation } = require("../validations");
 
 const router = require("express").Router();
 
@@ -8,5 +10,5 @@ router.get("/", (req, res) => {
   res.send("hiii in auth /");
 });
 
-router.post("/register", captcha.verify, authController.register);
+router.post("/register",[ captcha.verify,validate(authValidation.register)], authController.register);
 module.exports = router;
