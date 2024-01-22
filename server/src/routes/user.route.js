38e562549cf88ca2fa622a40a00485404ece98f9
AM-SagarThakare <sagar.thakare@angularminds.in>
2024-01-22 17:03:13 +0530

    get,update user api added

--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,3 +1,16 @@
+const { userController } = require("../controllers");
+const auth = require("../middlewares/auth");
+
 const router = require("express").Router();
+
+// Token authentication for all routes defined in this file
+router.use(auth());
+
+// get update user
+router
+  .route("/self")
+  .get(userController.getUser)
+  .patch(userController.updateUser);
+
 
 module.exports = router;
